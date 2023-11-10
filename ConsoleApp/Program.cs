4635275e15f12fde3985c1253e27cde107176af2
Martin Florio <martin@multitracks.com>
2023-11-10 15:48:51 -0300

    .

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ClassLibrary;
+
 Console.WriteLine("Hello, World!");
+
+Person person = new Person();
+
+person.Name = "Pepe";
+person.Age = 45;
